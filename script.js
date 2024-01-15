680a2706679ce6fb2505f1bb61c9ac1f41f4dc66
cleiton0a <126525336+cleiton0a@users.noreply.github.com>
2024-01-15 19:54:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 async function authenticate() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const usuario = {
-    usuario: username, senha: password
-  }
-  const headers = {
-    "Content-Type": "application/json", "accept": "application/json"
-  }
-  const response = await fetch("http://www.armariosapi.somee.com/api/Usuarios/Login",{ headers: headers, method: "POST", body: JSON.stringify(usuario)})
-  const responsejson = await response.json()
-  console.log(responsejson)
+  try {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const usuario = {
+      usuario: username,
+      senha: password
+    };
 
-  if (responsejson.token){
-    localStorage.setItem('token',responsejson.token)
-      window.location.href="/telalerqr"
+    const headers = {
+      "Content-Type": "application/json",
+      "accept": "application/json"
+    };
+
+    const response = await fetch("http://www.armariosapi.somee.com/api/Usuarios/Login", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(usuario)
+    });
+
+    if (!response.ok) {
+      
+      const errorMessage = await response.text();
+      throw new Error(`Error: ${response.status} - ${errorMessage}`);
+    }
+
+    const responsejson = await response.json();
+    console.log(responsejson);
+
+    if (responsejson.token) {
+      localStorage.setItem('token', responsejson.token);
+      window.location.href = "/telalerqr";
+    }
+  } catch (error) {
+    console.error('Error during authentication:', error);
   }
 }
 
-
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  authenticate()
-})
+  e.preventDefault();
+  authenticate();
+});
