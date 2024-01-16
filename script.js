1a53f5c763cb26c797859b1a2473d0270139a94b
cleiton0a <126525336+cleiton0a@users.noreply.github.com>
2024-01-16 20:10:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function authenticate() {
     };
 
     
-    const response = await fetch("http://www.armariosapi.somee.com/api/login", {
+    const response = await fetch("http://www.armariosapi.somee.com/api/Usuarios/Login", {
       method: "POST",
       headers: headers,
       body: JSON.stringify(usuario)
