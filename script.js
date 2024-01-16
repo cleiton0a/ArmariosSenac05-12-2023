7a323a7b772d3ada618c0302deddf04fee72bb84
cleiton0a <126525336+cleiton0a@users.noreply.github.com>
2024-01-15 21:31:06 -0300

    corrigido fetch script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 async function authenticate() {
   try {
-   
+
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
- 
+    
     const usuario = {
       usuario: username,
       senha: password
@@ -17,35 +16,41 @@ async function authenticate() {
       "accept": "application/json"
     };
 
- 
-    const response = await axios.post("http://www.armariosapi.somee.com/api/Usuarios/Login", usuario, {
-      headers: headers
+    
+    const response = await fetch("http://www.armariosapi.somee.com/api/Usuarios/Login", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(usuario)
     });
 
    
-    if (response.status !== 200) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    if (!response.ok) {
+      
+      const errorMessage = await response.text();
+      throw new Error(`Error: ${response.status} - ${errorMessage}`);
     }
 
     
-    const responsejson = response.data;
+    const responsejson = await response.json();
     console.log(responsejson);
 
-   
+    
     if (responsejson.token) {
       localStorage.setItem('token', responsejson.token);
       window.location.href = "/telalerqr";
     }
   } catch (error) {
-    console.error("Error during authentication:", error.message);
+    console.error('Error during authentication:', error);
   }
 }
 
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-  e.preventDefault(); 
+  e.preventDefault();
   authenticate();
 });
+
+
 
 
